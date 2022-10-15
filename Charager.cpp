0590aeab6814e3d4825dbbc09c7264aa2c7feec5
huge-pancake <huge.pancake.zxr@gmail.com>
2022-10-15 11:30:18 +0800

    style: format

--- Charager.cpp
+++ Charager.cpp
@@ -1,26 +1,26 @@
+#include "patchesforlinux.hpp"
 #include <cstdio>
 #include <cstdlib>
 #include <ctime>
-#include "patchesforlinux.hpp"
 
 // 1 millisecond = 1000 microsecond
 #define MS 1000
-#define S  1000000
+#define S 1000000
 
-#define WIDTH   40
-#define HEIGHT  15
-#define MV_LEFT  0
-#define MV_DOWN  1
-#define MV_UP    2
+#define WIDTH 40
+#define HEIGHT 15
+#define MV_LEFT 0
+#define MV_DOWN 1
+#define MV_UP 2
 #define MV_RIGHT 3
 
-#define BLOCK_VOID        0
-#define BLOCK_STONE       1
+#define BLOCK_VOID 0
+#define BLOCK_STONE 1
 #define BLOCK_STONE_SMALL 2
-#define BLOCK_TREE        3
-#define BLOCK_TREE_SMALL  4
-#define BLOCK_IRON        5
-#define BLOCK_GOLD        6
+#define BLOCK_TREE 3
+#define BLOCK_TREE_SMALL 4
+#define BLOCK_IRON 5
+#define BLOCK_GOLD 6
 
 class Player {
 private:
@@ -35,20 +35,20 @@ public:
     lastX = x;
     lastY = y;
     switch (direction) {
-      case MV_LEFT:
-        x - stepSize < 0 ? x = 0 : x -= stepSize;
-        break;
-      case MV_DOWN:
-        y + stepSize > HEIGHT - 1 ? y = HEIGHT - 1 : y += stepSize;
-        break;
-      case MV_UP:
-        y - stepSize < 0 ? y = 0 : y -= stepSize;
-        break;
-      case MV_RIGHT:
-        x + stepSize > WIDTH - 1 ? x = WIDTH - 1 : x += stepSize;
-        break;
-      default:
-        break;
+    case MV_LEFT:
+      x - stepSize < 0 ? x = 0 : x -= stepSize;
+      break;
+    case MV_DOWN:
+      y + stepSize > HEIGHT - 1 ? y = HEIGHT - 1 : y += stepSize;
+      break;
+    case MV_UP:
+      y - stepSize < 0 ? y = 0 : y -= stepSize;
+      break;
+    case MV_RIGHT:
+      x + stepSize > WIDTH - 1 ? x = WIDTH - 1 : x += stepSize;
+      break;
+    default:
+      break;
     }
   }
 };
@@ -66,12 +66,8 @@ Player player;
 // white        37         47
 // \033[1;36m{{TEXTS}}\033[0m
 
-void clearScreen() {
-  printf("\033[2J\033[1;1H");
-}
-int randomNumberIn(int min, int max) {
-  return rand() % (max - min + 1) + min;
-}
+void clearScreen() { printf("\033[2J\033[1;1H"); }
+int randomNumberIn(int min, int max) { return rand() % (max - min + 1) + min; }
 
 unsigned short _map[HEIGHT][WIDTH];
 
@@ -105,22 +101,22 @@ void resetMap() {
 
 char transformPoint(unsigned short pointCode) {
   switch (pointCode) {
-    case BLOCK_VOID:
-      return '.';
-    case BLOCK_STONE:
-      return 'S';
-    case BLOCK_STONE_SMALL:
-      return 's';
-    case BLOCK_TREE:
-      return 'T';
-    case BLOCK_TREE_SMALL:
-      return 't';
-    case BLOCK_IRON:
-      return 'Q';
-    case BLOCK_GOLD:
-      return 'Q';
-    default:
-      return '?';
+  case BLOCK_VOID:
+    return '.';
+  case BLOCK_STONE:
+    return 'S';
+  case BLOCK_STONE_SMALL:
+    return 's';
+  case BLOCK_TREE:
+    return 'T';
+  case BLOCK_TREE_SMALL:
+    return 't';
+  case BLOCK_IRON:
+    return 'Q';
+  case BLOCK_GOLD:
+    return 'Q';
+  default:
+    return '?';
   }
 }
 
@@ -135,20 +131,20 @@ void renderMap() {
       }
       _temp = _map[j][i];
       switch (_temp) {
-        case BLOCK_VOID:
-          printf("\033[1;0m%c\033[0m", transformPoint(_temp));
-          break;
-        case BLOCK_STONE:
-        case BLOCK_STONE_SMALL:
-        case BLOCK_GOLD:
-          printf("\033[1;33m%c\033[0m", transformPoint(_temp));
-          break;
-        case BLOCK_IRON:
-          printf("\033[1;37m%c\033[0m", transformPoint(_temp));
-          break;
-        case BLOCK_TREE:
-        case BLOCK_TREE_SMALL:
-          printf("\033[1;32m%c\033[0m", transformPoint(_temp));
+      case BLOCK_VOID:
+        printf("\033[1;0m%c\033[0m", transformPoint(_temp));
+        break;
+      case BLOCK_STONE:
+      case BLOCK_STONE_SMALL:
+      case BLOCK_GOLD:
+        printf("\033[1;33m%c\033[0m", transformPoint(_temp));
+        break;
+      case BLOCK_IRON:
+        printf("\033[1;37m%c\033[0m", transformPoint(_temp));
+        break;
+      case BLOCK_TREE:
+      case BLOCK_TREE_SMALL:
+        printf("\033[1;32m%c\033[0m", transformPoint(_temp));
       }
     }
     printf("          \n");
@@ -166,31 +162,31 @@ bool gameLoop() {
   if (kbhit()) {
     char _ = getch();
     switch (_) {
-      // movement
-      case 'h':
-      case 'a':
-        player.move(MV_LEFT);
-        break;
-      case 'j':
-      case 's':
-        player.move(MV_DOWN);
-        break;
-      case 'k':
-      case 'w':
-        player.move(MV_UP);
-        break;
-      case 'l':
-      case 'd':
-        player.move(MV_RIGHT);
-        break;
-      case 'r':
-        resetMap();
-        break;
-      case 'q':
-        flag = false;
-        break;
-      default:
-        break;
+    // movement
+    case 'h':
+    case 'a':
+      player.move(MV_LEFT);
+      break;
+    case 'j':
+    case 's':
+      player.move(MV_DOWN);
+      break;
+    case 'k':
+    case 'w':
+      player.move(MV_UP);
+      break;
+    case 'l':
+    case 'd':
+      player.move(MV_RIGHT);
+      break;
+    case 'r':
+      resetMap();
+      break;
+    case 'q':
+      flag = false;
+      break;
+    default:
+      break;
     }
   }
   update();
@@ -203,6 +199,7 @@ int main() {
   clearScreen();
   srand(time(NULL));
   resetMap();
-  while (gameLoop()) {}
+  while (gameLoop()) {
+  }
   return 0;
 }
--- patchesforlinux.hpp
+++ patchesforlinux.hpp
@@ -1,28 +1,28 @@
 #include <cstdio>
+#include <fcntl.h>
 #include <termios.h>
 #include <unistd.h>
-#include <fcntl.h>
- 
+
 int getch(void) {
   struct termios oldattr, newattr;
   int ch;
-  tcgetattr( STDIN_FILENO, &oldattr );
+  tcgetattr(STDIN_FILENO, &oldattr);
   newattr = oldattr;
-  newattr.c_lflag &= ~( ICANON | ECHO );
-  tcsetattr( STDIN_FILENO, TCSANOW, &newattr );
+  newattr.c_lflag &= ~(ICANON | ECHO);
+  tcsetattr(STDIN_FILENO, TCSANOW, &newattr);
   ch = getchar();
-  tcsetattr( STDIN_FILENO, TCSANOW, &oldattr );
+  tcsetattr(STDIN_FILENO, TCSANOW, &oldattr);
   return ch;
 }
 int getche(void) {
   struct termios oldattr, newattr;
   int ch;
-  tcgetattr( STDIN_FILENO, &oldattr );
+  tcgetattr(STDIN_FILENO, &oldattr);
   newattr = oldattr;
-  newattr.c_lflag &= ~( ICANON );
-  tcsetattr( STDIN_FILENO, TCSANOW, &newattr );
+  newattr.c_lflag &= ~(ICANON);
+  tcsetattr(STDIN_FILENO, TCSANOW, &newattr);
   ch = getchar();
-  tcsetattr( STDIN_FILENO, TCSANOW, &oldattr );
+  tcsetattr(STDIN_FILENO, TCSANOW, &oldattr);
   return ch;
 }
 
@@ -30,28 +30,25 @@ int kbhit(void) {
   struct termios oldt, newt;
   int ch;
   int oldf;
- 
+
   tcgetattr(STDIN_FILENO, &oldt);
   newt = oldt;
   newt.c_lflag &= ~(ICANON | ECHO);
   tcsetattr(STDIN_FILENO, TCSANOW, &newt);
   oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
   fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
- 
+
   ch = getchar();
- 
+
   tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
   fcntl(STDIN_FILENO, F_SETFL, oldf);
- 
-  if(ch != EOF)
-  {
+
+  if (ch != EOF) {
     ungetc(ch, stdin);
     return 1;
   }
- 
+
   return 0;
 }
 
-void gotoxy(int x, int y) {
-  printf("%c[%d;%df", 0x1B, y + 1, x + 1);
-}
+void gotoxy(int x, int y) { printf("%c[%d;%df", 0x1B, y + 1, x + 1); }
